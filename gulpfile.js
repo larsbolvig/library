d399319a17a4ec5e158ea43451ec186f73f17d8c
Lars Bolvig <lars.bolvig@steelseries.com>
2014-04-01 15:46:58 +0200

    Updates to css-library

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,13 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
+    runSequence = require('run-sequence'),
+    awspublish = require('gulp-awspublish'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
-    server = lr();
+    server = lr(),
+    libVersion = require('./package.json').version,
+    awsCreds = require('./aws_credentials.json');
 
 
 gulp.task('styles', function() {
@@ -16,6 +20,30 @@ gulp.task('styles', function() {
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
+
+gulp.task('publish', function() {
+
+  // create a new publisher
+  var publisher = awspublish.create({ key: awsCreds.key,  secret: awsCreds.secret, bucket: awsCreds.bucket });
+
+  // define custom headers
+  var headers = {
+     'Cache-Control': 'max-age=315360000, no-transform, public'
+     // ...
+   };
+
+  return gulp.src('dist/*.css')
+
+    // publisher will add Content-Length, Content-Type and Cache-Control headers
+    // and if not specified will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers));
+
+    // // create a cache file to speed up consecutive uploads
+    // .pipe(publisher.cache())
+
+    //  // print upload updates to console
+    // .pipe(awspublish.reporter());
+});
 
 
 gulp.task('watch', function() {
@@ -28,14 +56,28 @@ gulp.task('watch', function() {
 
 gulp.task('minify-css', function () {
   return gulp.src('css/app.css')
-    .pipe(rename({suffix: '.v0-0-1'}))
+    .pipe(rename({suffix: '.' + libVersion}))
     .pipe(minifycss())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('deploy', ['minify-css'], function() {
-  // Do stuff
+
+
+
+
+// gulp.task('deploy', ['minify-css', 'publish'], function() {
+//   // Do stuff
+// });
+
+
+gulp.task('deploy', function(callback) {
+  runSequence('minify-css',
+              /* ['build-scripts', 'build-styles'], */
+              'publish',
+              function () {
+                console.log("Library was successfully deployed to Amazon S3");
+              });
 });
 
 
