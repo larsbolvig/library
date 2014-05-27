f131572fd3063dfbbeefffffce6f1a55ae26ebae
Lars Bolvig <lars.bolvig@steelseries.com>
2014-05-27 16:36:45 +0200

    Add new plugins and rewrite tasks a bit

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     runSequence = require('run-sequence'),
     awspublish = require('gulp-awspublish'),
     livereload = require('gulp-livereload'),
+    bump = require('gulp-bump'),
     lr = require('tiny-lr'),
     server = lr(),
     libVersion = require('./package.json').version,
@@ -13,12 +14,22 @@ var gulp = require('gulp'),
 
 
 gulp.task('styles', function() {
-  return gulp.src('sass/app.scss')
+  return gulp.src('sass/static-lib.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'))
     .pipe(livereload(server))
     .pipe(notify({ message: 'Styles task complete' }));
 });
+
+gulp.task('bump', function(){
+  var options = {
+    type: 'prerelease'
+  };
+  gulp.src('./package.json')
+  .pipe(bump(options))
+  .pipe(gulp.dest('./'));
+});
+
 
 
 gulp.task('publish', function() {
@@ -34,15 +45,19 @@ gulp.task('publish', function() {
 
   return gulp.src('dist/*.css')
 
-    // publisher will add Content-Length, Content-Type and Cache-Control headers
-    // and if not specified will set x-amz-acl to public-read by default
-    .pipe(publisher.publish(headers));
+     // gzip, Set Content-Encoding headers and add .gz extension
+    .pipe(awspublish.gzip({ ext: '.gz' }))
 
-    // // create a cache file to speed up consecutive uploads
-    // .pipe(publisher.cache())
+    // publisher will add Content-Length, Content-Type and  headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers))
 
-    //  // print upload updates to console
-    // .pipe(awspublish.reporter());
+    // create a cache file to speed up consecutive uploads
+    .pipe(publisher.cache())
+
+     // print upload updates to console
+    .pipe(awspublish.reporter())
+    .pipe(notify({ message: 'Static lib was successfully deployed to S3' }));
 });
 
 
@@ -55,29 +70,16 @@ gulp.task('watch', function() {
 
 
 gulp.task('minify-css', function () {
-  return gulp.src('css/app.css')
+  console.log(libVersion);
+  return gulp.src('css/static-lib.css')
     .pipe(rename({suffix: '.' + libVersion}))
     .pipe(minifycss())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist'));
 });
 
-
-
-
-
-// gulp.task('deploy', ['minify-css', 'publish'], function() {
-//   // Do stuff
-// });
-
-
 gulp.task('deploy', function(callback) {
-  runSequence('minify-css',
-              /* ['build-scripts', 'build-styles'], */
-              'publish',
-              function () {
-                console.log("Library was successfully deployed to Amazon S3");
-              });
+  runSequence('bump', 'minify-css', 'publish');
 });
 
 
